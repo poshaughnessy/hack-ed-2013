2165aba7a347294528f665cbe98c87e6c3b67197
Peter O'Shaughnessy <p_os@yahoo.co.uk>
2013-04-24 12:20:38 +0100

    Committing

---
+++ js/app.js
@@ -0,0 +1,43 @@
+function loginUser() {
+    FB.login(function(response) { }, {scope:'email'});
+}
+
+function handleStatusChange(response) {
+    document.body.className = response.authResponse ? 'connected' : 'not_connected';
+    console.log(response);
+    if( response.authResponse ) {
+        updateUserInfo(response);
+    } else {
+        clearUserInfo();
+    }
+}
+
+function updateUserInfo(response) {
+    FB.api('/me', function(response) {
+        document.getElementById('user-info').innerHTML = '<img src="https://graph.facebook.com/' + response.id + '/picture">' + response.name;
+    });
+}
+
+function clearUserInfo() {
+    document.getElementById('user-info').innerHTML = '';
+}
+
+function getUserFriends() {
+    FB.api('/me/friends&fields=name,picture', function(response) {
+        console.log('Got friends: ', response);
+
+        if (!response.error) {
+            var markup = '';
+
+            var friends = response.data;
+
+            for (var i=0; i < friends.length && i < 25; i++) {
+                var friend = friends[i];
+
+                markup += '<img src="' + friend.picture + '"> ' + friend.name + '<br>';
+            }
+
+            document.getElementById('user-friends').innerHTML = markup;
+        }
+    });
+}
